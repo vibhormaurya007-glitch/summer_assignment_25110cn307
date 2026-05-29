3372ba9977a89800a038c421c9a310c19f06748f
vibhormaurya007-glitch <vibhormaurya007@gmail.com>
2026-05-29 21:30:56 +0530

    updated q4

--- DAY01/Q4.c
+++ DAY01/Q4.c
@@ -1,17 +1,24 @@
-//Write a program to Count digits in a number
+// Write a program to Count digits in a number
 
 #include <stdio.h>
 
-int main() {
-    int n,count=0;
+int main()
+{
+    int n, count = 0;
     printf("enter number:");
-    scanf("%d",&n);
-    while(n>0)
+    scanf("%d", &n);
+    if (n == 0)
     {
-        int temp=n%10;
-        n=n/10;
-        count++;
+        count = 1;
     }
-    printf("total number of digits:%d",count);
-    return 0;
-}
+        else
+        {
+            while (n > 0)
+            {
+                n = n / 10;
+                count++;
+            }
+        }
+        printf("total number of digits:%d", count);
+        return 0;
+    }
